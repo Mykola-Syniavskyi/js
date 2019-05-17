73d94e52fc898600128458f0d8d8f79860caa83a
Mykola-Syniavskyi <44326435+Mykola-Syniavskyi@users.noreply.github.com>
2019-05-17 17:22:50 +0300

    node post get

---
+++ node_post_get/index.js
@@ -0,0 +1,46 @@
+var port = 5000;
+var express = require('express');
+var bodyParser = require('body-parser');
+var server = express();
+ 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var DATA=[];
+
+server.use(bodyParser.json());
+
+//server.use(urlencodedParser)
+
+server.use('/', express.static('public'));
+
+server.get('/',urlencodedParser, (req, res) => {
+    
+    res.sendFile(__dirname + "/public/index.html");
+    //console.log(DATA);
+});
+
+
+
+
+
+server.post('/', urlencodedParser, (req, res) => {
+    DATA.push({text: req.body.text})
+    res.sendFile(__dirname + "/public/index.html");
+    
+    
+    console.log(DATA);
+});
+
+
+
+server.get('/index',urlencodedParser, (req, res) => {
+    
+    res.send(DATA); 
+ 
+     
+ });
+
+
+server.listen(port, () => {
+    console.log('Server runned on port:', port);
+});
